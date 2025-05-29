312976afa8fedebbe9dc3e5bddcd8e968bc4a8cf
Jayraj Singh Rathore <145545574+jayrajsinghrathore@users.noreply.github.com>
2025-05-29 14:21:27 +0530

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,7 +2,7 @@ import type { Config } from "tailwindcss";
  
 const config: Config = {   
     darkMode: ["class"], 
-    content: [
+    content: [ 
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
